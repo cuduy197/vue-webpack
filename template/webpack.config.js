7b199eca9cf0455fb122302001a5aac52c15e1f2
BUI KHUONG DUY <cuduy197@gmail.com>
2017-09-21 16:48:11 +0700

    Update webpack.config.js

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,84 +1,88 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require("path");
+var webpack = require("webpack");
+var ip = require("ip");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    filename: "build.js"
   },
   resolve: {
-    extensions: ['.js', '.vue'],
+    extensions: [".js", ".vue"],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      'public': path.resolve(__dirname, './public')
+      vue$: "vue/dist/vue.esm.js",
+      public: path.resolve(__dirname, "./public")
     }
   },
+
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
           loaders: {
-            {{#sass}}
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-            {{/sass}}
+            //sass
+            scss: "vue-style-loader!css-loader!sass-loader",
+            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax",
+            //js
+            js: "babel-loader"
           },
-          include: [
-            path.resolve(__dirname, './src'),
-            path.resolve(__dirname, './node_modules/vuetify')
-          ]
+          include: [path.resolve(__dirname, "./src"), path.resolve(__dirname, "./node_modules/vuetify")]
           // other vue-loader options go here
         }
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        include: [
-          path.resolve(__dirname, './src'),
-          path.resolve(__dirname, './node_modules/vuetify')
-        ]
+        loader: "babel-loader",
+        include: [path.resolve(__dirname, "./src"), path.resolve(__dirname, "./node_modules/vuetify")]
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          objectAssign: 'Object.assign'
+          objectAssign: "Object.assign"
         }
       },
       {
         test: /\.styl$/,
-        loader: ['style-loader', 'css-loader', 'stylus-loader']
+        loader: ["style-loader", "css-loader", "stylus-loader"]
       }
     ]
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    //noInfo: true,
+    host: ip.address()
   },
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
-}
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  devtool: "#cheap-source-map",
+  plugins: [
+    new webpack.ProvidePlugin({
+      mapState: ["vuex", "mapState"],
+      mapActions: ["vuex", "mapActions"],
+      mapGetters: ["vuex", "mapGetters"],
+      mapMutations: ["vuex", "mapMutations"]
+    })
+  ]
+};
+
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = false;
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: '"production"'
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
@@ -86,5 +90,5 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
-  ])
+  ]);
 }
